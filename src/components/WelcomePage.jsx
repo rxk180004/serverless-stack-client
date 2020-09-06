87d57dd9e21e2c49f6be7b8a0842a7bc9aca3d04
Ranjith Reddy Kommidi <ranjithreddykommidi@Ranjiths-MacBook-Pro.local>
2020-09-06 11:55:59 -0500

    First Commit

---
+++ src/components/WelcomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function WelcomePage() {
+    return(
+        <div>
+            <img className = "icon" src = "https://neilpatel.com/wp-content/uploads/2017/09/Pinterest.jpg"/>
+        </div>
+    )
+}
+
+export default WelcomePage;
---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "notes-app-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://crvsk9hy6j.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_8kIYex4qN",
+      APP_CLIENT_ID: "4ctke863051fufc2af5pjiso63",
+      IDENTITY_POOL_ID: "us-east-1:f72bf861-34bc-4db8-b675-bf3b130212b7"
+    }
+  };
